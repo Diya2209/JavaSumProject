58d934c608b507a1e368537d6f93b0bf5e42e54b
Diya <120104383+Diya2209@users.noreply.github.com>
2024-12-20 00:52:19 +0530

    Display the sum of the largest number

---
+++ SumLargestNumberr.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SumLargestNumberr {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the first number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the first number:");
+        int num3 = scanner.nextInt();
+
+        int largest = Math.max(num1, Math.max(num2, num3));
+
+        int sum = 0;
+        int temp = largest;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        System.out.println("The largest number is:" + largest);
+        System.out.println("The sum of the largest number is:" + sum);
+
+        scanner.close();
+    }
+
+}
